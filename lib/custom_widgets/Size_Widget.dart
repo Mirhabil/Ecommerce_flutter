2df617c05dc3c3566aed03421f6ca216b355f011
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-06-23 15:14:59 +0400

    Implement SizeWidget sizing feature

--- lib/custom_widgets/Size_Widget.dart
+++ lib/custom_widgets/Size_Widget.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SizeWidget extends StatelessWidget {
-  const SizeWidget({super.key});
+class SizeWidget extends StatefulWidget {
+  @override
+  State<SizeWidget> createState() => _SizeWidgetState();
+}
+
+class _SizeWidgetState extends State<SizeWidget> {
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,28 +17,43 @@ class SizeWidget extends StatelessWidget {
         itemCount: sizeList.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(left: index!=0 ? 22 : 0),
-            height: 48,
-            width: 48,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(9, 9),
-                  blurRadius: 16,
-                  color: Color(0xFF6295E2).withValues(alpha: 0.11),
-                ),
-                BoxShadow(
-                  offset: Offset(-9, -9),
-                  blurRadius: 16,
-                  color: Color(0xFFFFFFFF).withValues(alpha: 0.25),
-                ),
-              ],
-            ),
+          return GestureDetector(
+            child: Container(
+              margin: EdgeInsets.only(left: index != 0 ? 22 : 0),
+              height: 48,
+              width: 48,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: activeIndex == index ? Color(0xFF875B83) : Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(9, 9),
+                    blurRadius: 16,
+                    color: Color(0xFF6295E2).withValues(alpha: 0.11),
+                  ),
+                  BoxShadow(
+                    offset: Offset(-9, -9),
+                    blurRadius: 16,
+                    color: Color(0xFFFFFFFF).withValues(alpha: 0.25),
+                  ),
+                ],
+              ),
 
-            child: Center(child: Text(sizeList[index]),)
+              child: Center(
+                child: Text(
+                  sizeList[index],
+                  style: TextStyle(
+                    color:
+                        activeIndex == index ? Colors.white : Color(0xFF875B83),
+                  ),
+                ),
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                activeIndex = index;
+              });
+            },
           );
         },
       ),
